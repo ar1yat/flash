d3059a054812fa72859fdfcca4a3f6325269a7dc
ar1yat <ariat.tynyshbai@gmail.com>
2024-10-07 17:24:18 +0600

    Add files via upload

---
+++ RealDocument.java
@@ -0,0 +1,21 @@
+package Assignment2;
+
+// Реальный документ, который требует загрузки
+public class RealDocument implements Document {
+    private String title;
+
+    public RealDocument(String title) {
+        this.title = title;
+        loadFromDisk();
+    }
+
+    private void loadFromDisk() {
+        // Задача: Вывести сообщение о загрузке документа
+    }
+
+    @Override
+    public void display() {
+        // Задача: Вывести сообщение об отображении документа
+    }
+}
+
